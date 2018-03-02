9d275e4d255016dbb073a6fc146c1ffd912f6ff3
Danielly Costa <daniellycmcosta@gmail.com>
2018-03-02 07:48:02 -0300

    Retirando arquivos contendo lógica que não será mais utilizadas
      - Arquivos da 1ª versão da biblioteca

--- src/thirdyPartyLibs/deviceData/AccumData.cpp
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-#include "AccumData.h"
-
-void AccumData::setConsumption(float _consumption){
-  consumption = _consumption;
-}
-float AccumData::getConsumption(void){
-  return consumption;
-}
