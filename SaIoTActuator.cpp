67029ca02e0ba191620cfb1175b6a191fc9bdce6
PatricioOliveira <patricio@bct.ect.ufrn.br>
2018-03-16 19:14:08 -0300

    actuartor ok
    ...

--- SaIoTActuator.cpp
+++ SaIoTActuator.cpp
@@ -1,54 +1,66 @@
-// #include "SaIoTActuator.h"
-//
-//   void SaIoTActuator::setKey(String _key){
-//     key = _key;
-//   }
-//
-//   String SaIoTActuator::getKey(void){
-//     return key;
-//   }
-//
-//   void SaIoTActuator::setType(String _type){
-//     type = _type;
-//   }
-//
-//   String SaIoTActuator::getType(void){
-//     return type;
-//   }
-//
-//   void SaIoTActuator::setLabel(String _label){
-//     label = _label;
-//   }
-//
-//   String SaIoTActuator::getLabel(void){
-//     return label;
-//   }
-//
-//   void SaIoTActuator::setInterval(double _min,double _step,double _max){
-//     min = _min;
-//     step = _step;
-//     max = _max;
-//   }
-//
-//   void SaIoTActuator::setMin(double _min){
-//      min = _min;
-//   }
-//   void SaIoTActuator::setMax(double _max){
-//     max = _max;
-//   }
-//
-//   void SaIoTActuator::setStep(double _step){
-//     step = _step;
-//   }
-//
-//   double SaIoTActuator::getMin(void){
-//     return min;
-//   }
-//
-//   double SaIoTActuator::getMax(void){
-//     return max;
-//   }
-//
-//   double SaIoTActuator::getStep(void){
-//     return step;
-//   }
+#include "SaIoTActuator.h"
+
+  void SaIoTActuator::setKey(String _key){
+  size_t size = _key.length();
+  for (size_t a=0; a<=64 && a<=size ;a++)
+    {
+      key[a] = char(_key[a]);
+    }
+  }
+
+  String SaIoTActuator::getKey(void){
+    return key;
+  }
+
+  void SaIoTActuator::setType(String _type){
+  	size_t size = _type.length();
+  	for (size_t a=0; a<=64 && a<=size ;a++)
+      {
+        type[a] = char(_type[a]);
+      }
+  }
+
+  String SaIoTActuator::getType(void){
+    return type;
+  }
+
+void SaIoTActuator::setLabel(String _label){
+  size_t size = _label.length();
+  for (register size_t a=0; a<=64 && a<=size ;a++)
+  {
+    label[a] = char(_label[a]);
+  }
+  }
+
+  String SaIoTActuator::getLabel(void){
+    return label;
+  }
+
+  void SaIoTActuator::setInterval(double _min,double _step,double _max){
+    min  = _min;
+    step = _step;
+    max  = _max;
+  }
+
+  void SaIoTActuator::setMin(double _min){
+     min = _min;
+  }
+  void SaIoTActuator::setMax(double _max){
+    max = _max;
+  }
+
+  void SaIoTActuator::setStep(double _step){
+    step = _step;
+  }
+
+  double SaIoTActuator::getMin(void){
+    return min;
+  }
+
+  double SaIoTActuator::getMax(void){
+    return max;
+  }
+
+  double SaIoTActuator::getStep(void){
+    return step;
+  }
--- SaIoTActuator.h
+++ SaIoTActuator.h
@@ -1,34 +1,34 @@
-// #ifndef SaIoTActuator_h
-// #define SaIoTActuator_h
-//
-// #include<Arduino.h>
-//
-// class SaIoTActuator{
-//     private:
-//       char key[50] = "Not setted";
-//       char type[50] = "Not setted";
-//       char label[50] = "Not setted";
-//
-//       double min=0,max=1,step=0.01;
-//     public:
-//         void setKey(String _key);
-//         String getKey(void);
-//
-//         void setType(String _type);
-//         String getType(void);
-//
-//         void setLabel(String _label);
-//         String getLabel(void);
-//
-//         void setInterval(double _min,double _step,double _max);
-//
-//         void setMin(double _min);
-//         double getMin(void);
-//
-//         void setMax(double _max);
-//         double getMax(void);
-//
-//         void setStep(double _step);
-//         double getStep(void);
-// };
-// #endif
+#ifndef SaIoTActuator_h
+#define SaIoTActuator_h
+
+#include<Arduino.h>
+
+class SaIoTActuator{
+    private:
+      char key[50] = "Not setted";
+      char type[50] = "Not setted";
+      char label[50] = "Not setted";
+
+      double min=0,max=1,step=0.01;
+    public:
+        void setKey(String _key);
+        String getKey(void);
+
+        void setType(String _type);
+        String getType(void);
+
+        void setLabel(String _label);
+        String getLabel(void);
+
+        void setInterval(double _min,double _step,double _max);
+
+        void setMin(double _min);
+        double getMin(void);
+
+        void setMax(double _max);
+        double getMax(void);
+
+        void setStep(double _step);
+        double getStep(void);
+};
+#endif
