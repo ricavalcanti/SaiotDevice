72730313df722a303aeb9662730188511a0846f7
Danielly Costa <daniellycmcosta@gmail.com>
2018-05-03 21:40:58 -0300

    início da implem. dos controllers (ver descrição)
    Inclusão da variável description,
    mudança do nome da variável label p/ tag,
    implementação dos métodos da classe usando Strings
    comentando variáveis que possívelmente não continuarão na classe (min,
    max, step)

--- SaIoTController.cpp
+++ SaIoTController.cpp
@@ -1,73 +1,86 @@
-// #include "SaIoTController.h"
+#include "SaIoTController.h"
 
-// SaIoTController::SaIoTController(String _key, String _type, String _label, double _min, double _step, double _max){
-//   setKey(_key);
-//   setType(_type);
-//   setLabel(_label);
-//   setInterval(_min,_step,_max);
-// }
+SaIoTController::SaIoTController(String _key, String _type, String _label, String _description){//, double _min, double _step, double _max){
+  setKey(_key);
+  setType(_type);
+  setLabel(_label);
+  setDescription(_description);
+  //setInterval(_min,_step,_max);
+}
 
-//   void SaIoTController::setKey(String _key){
-//   size_t size = _key.length();
-//   for (size_t a=0; a<=64 && a<=size ;a++)
-//     {
-//       key[a] = char(_key[a]);
-//     }
-//   }
+  void SaIoTController::setKey(String _key){
+    key = _key;
+  // size_t size = _key.length();
+  // for (size_t a=0; a<=64 && a<=size ;a++)
+  //   {
+  //     key[a] = char(_key[a]);
+  //   }
+  }
 
-//   String SaIoTController::getKey(void){
-//     return key;
-//   }
+  String SaIoTController::getKey(void){
+    return key;
+  }
 
-//   void SaIoTController::setType(String _type){
-//   	size_t size = _type.length();
-//   	for (size_t a=0; a<=64 && a<=size ;a++)
-//       {
-//         type[a] = char(_type[a]);
-//       }
-//   }
+  void SaIoTController::setType(String _type){
+    type = _type;
+  	// size_t size = _type.length();
+  	// for (size_t a=0; a<=64 && a<=size ;a++)
+    //   {
+    //     type[a] = char(_type[a]);
+    //   }
+  }
 
-//   String SaIoTController::getType(void){
-//     return type;
-//   }
+  String SaIoTController::getType(void){
+    return type;
+  }
 
-// void SaIoTController::setLabel(String _label){
-//   size_t size = _label.length();
-//   for (size_t a=0; a<=64 && a<=size ;a++)
-//   {
-//     label[a] = char(_label[a]);
-//   }
-//   }
+void SaIoTController::setTag(String _tag){
+  tag = _tag;
+  // size_t size = _label.length();
+  // for (size_t a=0; a<=64 && a<=size ;a++)
+  // {
+  //   label[a] = char(_label[a]);
+  // }
+  }
 
-//   String SaIoTController::getLabel(void){
-//     return label;
-//   }
+  String SaIoTController::getTag(void){
+    return tag;
+  }
 
-//   void SaIoTController::setInterval(double _min,double _step,double _max){
-//     min  = _min;
-//     step = _step;
-//     max  = _max;
-//   }
+  
+void SaIoTController::setDescription(String _description){
+  description = _description;
+}
 
-//   void SaIoTController::setMin(double _min){
-//      min = _min;
-//   }
-//   void SaIoTController::setMax(double _max){
-//     max = _max;
-//   }
+String SaIoTController::getDescription(void){
+  return description;
+}
 
-//   void SaIoTController::setStep(double _step){
-//     step = _step;
-//   }
+  // void SaIoTController::setInterval(double _min,double _step,double _max){
+  //   min  = _min;
+  //   step = _step;
+  //   max  = _max;
+  // }
 
-//   double SaIoTController::getMin(void){
-//     return min;
-//   }
+  // void SaIoTController::setMin(double _min){
+  //    min = _min;
+  // }
+  // void SaIoTController::setMax(double _max){
+  //   max = _max;
+  // }
 
-//   double SaIoTController::getMax(void){
-//     return max;
-//   }
+  // void SaIoTController::setStep(double _step){
+  //   step = _step;
+  // }
 
-//   double SaIoTController::getStep(void){
-//     return step;
-//   }
+  // double SaIoTController::getMin(void){
+  //   return min;
+  // }
+
+  // double SaIoTController::getMax(void){
+  //   return max;
+  // }
+
+  // double SaIoTController::getStep(void){
+  //   return step;
+  // }
--- SaIoTController.h
+++ SaIoTController.h
@@ -1,39 +1,44 @@
-// #ifndef SaIoTController_h
-// #define SaIoTController_h
+#ifndef SaIoTController_h
+#define SaIoTController_h
 
-// #include<Arduino.h>
+#include<Arduino.h>
 
 
-// class SaIoTController{
-//     private:
-//       char key[5] = "";
-//       char type[20] = "";
-//       char label[50] = "Not setted";
+class SaIoTController{
+    private:
+      char key[5] = "";
+      char type[20] = "";
+      char tag[50] = "Not setted";
+      char description[150] ="Not setted";
 
-//       double min=0,max=1,step=0.01;
-//     public:
-//         SaIoTController();
-//         SaIoTController(char* _key, char* _type, char* _label, double _min, double _step, double _max);
-//         ~SaIoTController();
+      //VAI CONTINUAR TENDO ESSAS VARIÁVEIS NO CONTROLLER?
+      //double min=0,max=1,step=0.01;
+    public:
+        SaIoTController();
+        SaIoTController(String _key, String _type, String _tag, String _description); //, double _min, double _step, double _max);
+        ~SaIoTController();
 
-//         void setKey(char* _key);
-//         char* getKey(void);
+        void setKey(String _key);
+        String getKey(void);
 
-//         void setType(char* _type);
-//         char* getType(void);
+        void setType(String _type);
+        String getType(void);
 
-//         void setLabel(char* _label);
-//         char* getLabel(void);
+        void setTag(String _tag);
+        String getTag(void);
 
-//         void setInterval(double _min,double _step,double _max);
+        void setDescription(String _description);
+        String getDescription(void);
 
-//         void setMin(double _min);
-//         double getMin(void);
+        // void setInterval(double _min,double _step,double _max);
 
-//         void setMax(double _max);
-//         double getMax(void);
+        // void setMin(double _min);
+        // double getMin(void);
 
-//         void setStep(double _step);
-//         double getStep(void);
-// };
-// #endif
+        // void setMax(double _max);
+        // double getMax(void);
+
+        // void setStep(double _step);
+        // double getStep(void);
+};
+#endif
